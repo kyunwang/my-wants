76b3160424b8b9226ae01b594fa333c8ee56b5f8
Kangyun Wang <wky@kangyuns-mbp.home>
2018-03-11 12:48:38 +0100

    Add colors

---
+++ app/constants/Color.js
@@ -0,0 +1,12 @@
+export default {
+	primaryColor: '#6CACF7', // Blue tint
+	mainBlack: '#3F3F3F', // Main text black
+	secColor: '#9B9B9B', // Secondary text grey
+
+	secText: '#9B9B9B', // Darkish grey
+
+	backgroundColor: '#F6F6F9', // Light creme white color
+
+	lightGrey: '#E7E7E7', // For the unfilled bar
+};
+
