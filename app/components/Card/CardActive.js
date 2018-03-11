cd201f69e69c9ee27aa947f8955c6a15a21d597b
Kangyun Wang <wky@kangyuns-mbp.home>
2018-03-11 12:48:52 +0100

    Correct import

--- app/components/Card/CardActive.js
+++ app/components/Card/CardActive.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
-import Color from '../../constants/Colors';
+import Color from '../../constants/Color';
 
 const CardActiveWrapper = styled.View`
 	min-height: 100;
