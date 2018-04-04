1674ba6bc08fb6b7009956a57ab0d89a86ca1c0d
Kangyun Wang <wky@kangyuns-mbp.home>
2018-04-04 20:10:13 +0200

    Added basic styling to textinput for forms

--- app/components/UI/Form/TextInput.js
+++ app/components/UI/Form/TextInput.js
@@ -3,25 +3,53 @@ import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
+import Color from '../../../constants/Color';
+
+const InputContainer = styled.View`
+	flex: 1;
+	max-height: 55px;
+	margin-bottom: 12px;
+	border-bottom-width: 2px;
+	border-bottom-color: ${Color.lightGrey};
+	padding: 0 6px;
+`;
+
 const StyledTextInput = styled.TextInput`
 	font-family: font-regular;
-	width: 100%;
+	font-size: 18px;
 	height: 30px;
+	width: 100%;
 `;
 
 const Label = styled.Text`
 	font-family: font-regular;
+	color: ${Color.primaryColor}
 `;
 
 
 class TextInput extends Component {
+	static propTypes = {
+		labelName: PropTypes.string.isRequired,
+	}
+
+	static defaultProps = {
+		// labelName: 'label',
+	}
+
+	state = {}
+
 	render() {
-	 return (
-			<Fragment>
-			<Label>Label</Label>
-			<StyledTextInput maxLength={20} />
-  </Fragment>
-	 );
+		const {
+			containerStyle,
+			labelName,
+		} = this.props;
+
+		return (
+			<InputContainer style={containerStyle}>
+				<Label>{labelName}</Label>
+				<StyledTextInput maxLength={20} />
+			</InputContainer>
+		);
 	}
 }
 
